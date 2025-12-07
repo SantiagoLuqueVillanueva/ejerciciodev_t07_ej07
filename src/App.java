4e848bfd551d4c5de7531c28067da0fab5d6f0c9
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-12-07 10:23:20 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] num = new int[100];
+
+        for (int i = 0; i < num.length; i++) {
+            num[i] = (int)(Math.random()*21);
+        }
+
+        for (int i = 0; i < num.length; i++) {
+            System.out.print((num[i]) + " ");
+        }
+
+        System.out.println();
+
+        int elegido = Integer.parseInt(System.console().readLine("Introduzca un número de los que se han mostrado: "));
+        int nuevo = Integer.parseInt(System.console().readLine("Introduzca el valor por el que quiere sustituirlo: "));
+
+        System.out.println();
+
+        for (int i = 0; i < num.length; i++) {
+            System.out.print(num[i]==elegido?nuevo:num[i]);
+        }
     }
 }
